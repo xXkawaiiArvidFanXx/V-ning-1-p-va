fc0285d6c2b0ae633345d8783d2140c8fb0dc8e8
frej <frejlars.svedenmark@skola.taby.se>
2025-11-26 12:32:50 +0100

    Rum modul med Karta (inte turtle)

---
+++ map_module.py
@@ -0,0 +1,31 @@
+#Här skapas en karta och rummen slumpas fram
+#karta grid lista i lista 
+#map = kartan
+#Börja med map creation för att skapa kartan
+#med funktionen Print_map() så kan du skriva ut kartan i terminalen
+import random as rand
+
+def Map_Creation():
+    map = []
+    map_size = 5 #bestämmer storlek på kartan
+    for i in range (map_size): #En tom kart mall skapas
+        map.append([0,0,0,0,0])
+
+    for a in range (map_size):
+        for b in range (map_size): #här slumpas rummens egenskaper fram N = Neutralt 
+            #G = gott/GOOd O = Ont/OEvil   T = Trap/fälla kanske R = Renoveras B = BOSS
+            Room_Type = ["N","G","O","T"]
+
+            map[a][b]=(Room_Type[rand.randint(0,3)])
+
+    #Bossmodul
+    #Alltid en boss 1 i mittenrummet 
+    #Boss 2 är alltid i sista rummet???
+    map[2][2] = "B"
+    return map
+def Print_map():
+    for row in map: #Hur man kan skiva ut kartan fint
+        print(row)
+
+map = Map_Creation()
+Print_map()
