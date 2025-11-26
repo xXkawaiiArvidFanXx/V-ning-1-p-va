cb71e1cacbe3b56188dc64486a104e73fbabde3a
Elias <elias.kronlid@skola.taby.se>
2025-11-26 12:50:58 +0100

    ändra lite i maingame la till start funkis

--- maingame.py
+++ maingame.py
@@ -1,3 +1,12 @@
 # här ska vi skapa vår huvuddel av spelet och imporera klasser etc eftersom vi jobbar
-from player import *
-from loading import *
+# from player import *
+# from loading import *
+# from map_module import * 
+# from soundengien import *
+# importera där de behövs annar kommer de printas i start
+
+
+
+
+def startgame(): #starten till spelet, här ska man välja om man ska skapa en ny sparfil eller om man vill importera en sparad version
+    print("Välkommen till första våningen på åva")
