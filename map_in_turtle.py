d4b7e572afd87dcc77cf1b10c91aa8f7d1932dfe
frej <frejlars.svedenmark@skola.taby.se>
2025-11-28 11:05:18 +0100

    Karta

--- map_in_turtle.py
+++ map_in_turtle.py
@@ -4,11 +4,34 @@
 # man ska se vart man är 
 import turtle as t
 from map_module import *
+Print_map()
+def Write_room():
+    t.pendown()
+    for i in range (4):
+        t.forward(60)
+        t.right(90)
+    t.penup()
 
 def Turtle_maps():
-    Map = t.Turtle()
+    #Map = t.Turtle()
     t.penup()
-    t.goto(-300,100)
+    t.goto(-300,200)
     # en for loop som går igenom alla rum 4*7 och skriver ut Vad som finns i dem (täks med ? om ett tag)
+    antal_rum=0
+    t.speed(100000)
+    for a in range (1,5):
+        for i in range (7):
+            Write_room()
+            antal_rum +=1
+            if antal_rum < 7:
+                t.forward(80)
+            else:
+                t.goto(-300,(200-a*75))#vart den nu ska gå tillbaka till början fast längre ned 
+                antal_rum = 0
+        
+
 
     t.done()
+
+
+Turtle_maps()
