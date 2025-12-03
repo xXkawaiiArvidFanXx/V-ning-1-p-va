80c9d5cd6b9c0e5c475c95cd51d6f54130bb5975
Filip_Andersson <08andfil@taby.se>
2025-12-03 12:08:09 +0100

    commit

--- fight_test.py
+++
@@ -1,9 +0,0 @@
-from player import *
-from fight import *
-
-player = Player(20, 5, "Hero", 10)
-
-enemy = Monster(15, 3, "Goblin", 20)
-fight(player, enemy)    
-
-#ta bort när spelet är klart
--- map_module.py
+++ map_module.py
@@ -4,7 +4,7 @@
 #Börja med map creation för att skapa kartan
 #med funktionen Print_map() så kan du skriva ut kartan i terminalen
 import random as rand
-
+from player import *
 def Map_Creation():
     map = []
     map_size = 7 #bestämmer storlek på kartan
@@ -28,25 +28,25 @@ def Print_map():
     for row in map: #Hur man kan skiva ut kartan fint
         print(row)
 
-map = Map_Creation()
-
-def player_position(player_pos_y, player_pos_x, player_move):
+def update_player_position(player_pos_y, player_pos_x, player_move):
+    """Uppdaterar spelarens position baserat på deras rörelse och position"""
     while True:
         if player_move == "up":
-            player_pos_y -= 1
-        elif player_move == "down":
             player_pos_y += 1
+        elif player_move == "down":
+            player_pos_y -= 1
         elif player_move == "left":
             player_pos_x -= 1
         elif player_move == "right":
             player_pos_x += 1
+
         else:
             print("ogiltig riktning")
             continue
-        break
-    return player_pos_y, player_pos_x
-
+        return player_pos_y, player_pos_x
+    
 def get_room_type(player_pos_y, player_pos_x):
+    """Returnerar vilken typ av rum spelaren är i baserat på deras position"""
     room = (map[player_pos_y][player_pos_x])
 
     if room == "N":
@@ -59,3 +59,16 @@ def get_room_type(player_pos_y, player_pos_x):
         return "Fällrum"
     elif room == "B":
         return "Bossrum"
+    elif room == "E":
+        return "Tomt rum"
+    
+
+#test spelare, se till att ta bort den när vi använder detta i maingame.py
+
+map = Map_Creation()
+
+#Lägg in detta i maingame.py med spelare så kommer det funka, funkar inte i denna fil eftersom det inte finns någon player  inte finns här
+
+# player.pos_y, player.pos_x = update_player_position(player.pos_y, player.pos_x, "down") detta är för att flytta spelaren
+
+# get_room_type(player.pos_y, player.pos_x) detta är för att kolla vilket rum spelaren är i och för att
