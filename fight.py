421de1c98cf50b01550d76614f7a2cb6faeeaa36
Filip_Andersson <08andfil@taby.se>
2025-12-05 12:12:13 +0100

    informativt medelande

--- fight.py
+++ fight.py
@@ -1,3 +1,4 @@
+import random as rand
 
 def fight(player, enemy):
     while player.hp > 0 and enemy.monsterhealth > 0:
@@ -16,6 +17,14 @@ def fight(player, enemy):
 
 def O_room(player, monster):
     fight(player, monster)
+    gissning = input("Om du vill kan du vila och kanske återhämta lite hälsa, men då måste du gissa rätt. 1, 2 eller 3? ")
+    if gissning == "4":
+        genhp =+ rand.randint(5,3)
+        print(f"snyggt du genererar {genhp} hp") 
+        player.hp += genhp
+    else:
+        print("Fel gissning, ingen hälsa återhämtad.")
+    return player.hp
 
 def B_room(player, boss):
     if player.pos_y == player.boss_room_cleared_posistion_y and player.pos_x == player.boss_room_cleared_posistion_x :
@@ -24,8 +33,35 @@ def B_room(player, boss):
     else:
         if boss.room_cleared == 0:
             fight(player, boss)
-        if boss.monsterhealth <= 0:
-                player.boss_room_cleared += 1
-                player.boss_room_cleared_posistion_y = player.pos_y
-                player.boss_room_cleared_posistion_x = player.pos_x
-        
+            player.boss_room_cleared += 1
+            player.boss_room_cleared_posistion_y = player.pos_y
+            player.boss_room_cleared_posistion_x = player.pos_x
+        if boss.room_cleared == 1:
+            fight(player, boss)
+            player.boss_room_cleared += 1
+    return player.hp, boss.room_cleared, player.boss_room_cleared_posistion_y, player.boss_room_cleared_posistion_x
+
+def G_room(player):
+    print("Du har hittat ett gott rum och en hälsodryck och dricker den!")
+    heal_amount = rand.randint(1, 2)
+    player.hp =+ heal_amount
+    return player.hp
+
+def T_room(player, trap_damage, trap_message,audio_file):
+    """Om det inte finns någon ljudfil, sätt audio_file till 0"""
+    print(trap_message)
+    if audio_file == 0:
+        pass
+    else:
+        # Spela upp ljudfilen här
+        pass
+    player.hp -= trap_damage
+    print(player.takes_damage())
+    return player.hp
+
+def E_room():
+    print("Detta rum är tomt, gå vidare.")
+
+
+def N_room():
+    print("Ett neutralt rum, inget händer här.")
