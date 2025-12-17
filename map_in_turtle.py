551e7d6175303c29fe9181140c04c8684d5d1972
frej <frejlars.svedenmark@skola.taby.se>
2025-12-17 11:49:59 +0100

    committar lite

--- map_in_turtle.py
+++ map_in_turtle.py
@@ -30,6 +30,7 @@ def Turtle_maps(x,y): #kanske en lista för icke hittade rum
     t.penup()
     t.speed(-1)
     t.hideturtle()
+    
     # en for loop som går igenom alla rum 4*7 och skriver ut Vad som finns i dem (täks med ? om ett tag)
     for a in range (1,5):
         for i in range (7):
@@ -52,7 +53,7 @@ def Turtle_maps(x,y): #kanske en lista för icke hittade rum
         if action == "Y":
            Turtle_maps(player.pos_x, player.pos_y)
         elif action == "q":
-            t.done()
+            #t.done()
             break
         else:
             deadahh()
