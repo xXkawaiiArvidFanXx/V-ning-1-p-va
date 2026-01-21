e39ea8716ee4ebf4743c2c00a444bb7844cf56d8
Filip_Andersson <08andfil@taby.se>
2026-01-21 12:28:04 +0100

    fixade lite text grejer

--- maingame.py
+++ maingame.py
@@ -16,13 +16,11 @@ def class_chooser():
         buffered_type("""1. Grisch har ett bälte som de kan använda som vapen, men de kan välja att springa ifrån en fiende fast då förlorar de aura och tappar byxorna. 
 Däremot är grischpojken överdrivet självsäker som kanske kan komma till nytta. Grisch har Aura istället för Hp""", 0.01) #Självsäkerheten gör absolut ingenting, vi vill bara att man ska välja den sänsta karaktären.
         time.sleep(1)
-        buffered_type("""2. Estet har en gitarr som de kan använda för att slå till sina fiender. Gitarren kan man använda för att höja sin karisma men om du använder gitarren för att slåss kommer den gå sönder. 
-Om du försöker spela med en sönder gitarr kommer du tappa allt förutom en karisma poäng. Du är inte erfaren i gitarr eftersom att du endast har övat att spela trumpet hela ditt liv""", 0.01)
+        buffered_type("""2. Estet har en gitarr som de kan använda för att slå till sina fiender. Eftersom du har en gitarr har du hög karisma även fast du inte är en erfaren gitarr spelare då du endast har övat att spela trumpet hela ditt liv""", 0.01)
         time.sleep(1)
         buffered_type("3. Rektorn har en dator som kan användas för att skriva hem mejl, funkar för alla fiender (gör så att dom tar skada) men chans att stöta på tekniska problem och därmed förlorar man sin dator och fighten mot fienden", 0.01)
         time.sleep(1)
-        buffered_type("""4. Lärare har fattat att pennan är mäktigare än svärdet. Speciellt när pennorna är vässade. 
-Men efter 3 användningar kan inte pennan användas utan att vässas pennans udd ska kunna gå sönder och pennan kan ha vässats sönder, då måste man spendera 2 rundor på att få tillbaka udden""", 0.01)
+        buffered_type("""4. Lärare har fattat att pennan är mäktigare än svärdet. Speciellt när pennorna är vässade. Då du är en chill lärare så har du också relativt hög karisma""", 0.01)
         time.sleep(1)
 
         buffered_type("Välj nu noga vilken karaktär du väljer. Det kan komma att bli skillnaden mellan att bli en hjälte eller ett misslyckande", 0.01)
